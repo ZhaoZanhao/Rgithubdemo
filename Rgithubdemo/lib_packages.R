82d3f03c816238f800750b30e67c88e861b01455
Zanhao Zhao <zanhaozhao@163.com>
2020-06-26 20:50:17 +0800

    test6

---
+++ Rgithubdemo/lib_packages.R
@@ -0,0 +1,48 @@
+install.packages(c('flexdashboard'), destdir = "D:/Program/R/Rpackages")
+install.packages(c('DT', 'readxl', 'dplyr'), destdir = "D:/Program/R/Rpackages")
+install.packages(c('plotly'), destdir = "D:/Program/R/Rpackages")
+install.packages(c('devtools'), destdir = "D:/Program/R/Rpackages")
+install.packages(c('reshape2'), destdir = "D:/Program/R/Rpackages")
+install.packages(c('ggplot2'), destdir = "D:/Program/R/Rpackages")
+install.packages(c('plotrix'), destdir = "D:/Program/R/Rpackages")
+
+install.packages(c('packrat', 'rsconnect'), destdir = "D:/Program/R/Rpackages")
+
+install.packages(c('languageserver'), destdir = "D:/Program/R/Rpackages")
+
+
+install.packages(c('lubridate'), destdir = "D:/Program/R/Rpackages")
+devtools::install_github("lchiffon/REmap")
+
+install.packages(c('tidyverse'), destdir = "D:/Program/R/Rpackages")
+
+
+install.packages(c('jsonlite'), destdir = "D:/Program/R/Rpackages")
+
+library(jsonlite)
+
+library(flexdashboard)
+library(DT)
+library(readxl)
+library(dplyr)
+library(rmarkdown)
+library(plotly)
+
+
+library(devtools)
+library(REmap)
+library(reshape2)
+library(ggplot2)
+library(plotrix)
+library(shiny)
+library(lubridate)
+
+library(packrat)
+library(rsconnect)
+library(languageserver)
+
+a<-1
+
+
+
+
